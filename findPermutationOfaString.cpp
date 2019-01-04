7d138b56149aba00db75dbac76f3d0b63482f770
Sushma Macha <30306056+SushmaMacha@users.noreply.github.com>
2019-01-04 00:03:06 -0800

    Update findPermutationOfaString.cpp

--- findPermutationOfaString.cpp
+++ findPermutationOfaString.cpp
@@ -1,37 +1,25 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        int l = s2.length(),s = 0,l1 = s1.length();
-        char c[l1+1];
-        char *c1;
-        s1.copy(c,l1);
-        c[l1] = '\0';
-        string str(c);
-       if(l == 0 || l1 > l)
-           return false;
-       for(int i = 0;i < l;i++)
-       {
-           //check for continuity
-           //c1 = strchr(c,s2[i]);
-           std::size_t f = str.find(s2[i]);
-           if(f != std::string::npos)
-           {
-               s++;
-               str.erase(f,1);
-               //cout << str << endl;
-               if(str.length() == 0)
-                   return true;
-               continue;
-           }
-           else if(s > 0)
-           {
-               s1.copy(c,l1);
-               str = c;
-               i = i-s;
-               //cout << "copy " << i << endl;
-               s = 0;
-           }
-       }
+     int count[256] = {0},l1,l2;
+        l1 = s1.length(); l2 = s2.length();
+        for(char c : s1)
+            count[c]++;
+        for(int i = 0,j=0; i < l2; i++)
+        {
+            if(count[s2[i]] > 0){
+                l1--;
+            }
+            count[s2[i]]--;
+            while(l1 == 0)
+            {
+                if((i+1)-j == s1.length())
+                    return true;
+                if(++count[s2[j++]] > 0)
+                    l1++;
+            }
+        }
+        
         return false;
     }
 };
