f70ef0423967202ca5981577ef101084530215ff
Sushma Macha <30306056+SushmaMacha@users.noreply.github.com>
2019-01-06 22:55:15 -0800

    Update MoutainPeak.cpp

--- MoutainPeak.cpp
+++ MoutainPeak.cpp
@@ -3,14 +3,28 @@
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& A) {
-        int j = A.size(),i=0,r=0;
+        int j = A.size(),s = 0,e = j-1,mid = j/2,i=0;
         if(j < 3)
-            return false;
-        while(A[i] < A[i+1] && i+1 < j) i++;
-        r=i;
-        while(A[i] > A[i+1] && i+1 < j) i++;
-        if(i == j-1)
-            return r;
-        return -1;
+            return -1;
+        while(1)
+        {
+            mid = (s+e)/2;
+            if(A[mid] < A[mid+1])
+                s = mid;
+            else
+                e = mid;
+            if(e-s <= 1)
+                return e;
+        }
+        return e;
+        
+        // if(j < 3)
+        //     return false;
+        // while(A[i] < A[i+1] && i+1 < j) i++;
+        // r=i;
+        // while(A[i] > A[i+1] && i+1 < j) i++;
+        // if(i == j-1)
+        //     return r;
+        //return -1;
     }
 };
