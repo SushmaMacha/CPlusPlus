427b923b0f16ee4be384ff2713f9631783f395d4
Sushma Macha <30306056+SushmaMacha@users.noreply.github.com>
2019-01-07 21:54:00 -0800

    Update rotateArray.cpp

--- rotateArray.cpp
+++ rotateArray.cpp
@@ -1,43 +1,34 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        if(k==0)
+         if(k==0)
             return;
-        int s = nums.size();
+        int s = nums.size(),i=0,j=0,t=0,temp=nums[0];
         if(k > s)
             k = k-s;
-         int temp = 0,i=0,a=s/k,t=k,m=2,j=0;
-        while(a > 0)
-        {
-            t = k;j=m*k;
-            if(s > j)
+        //cout << k << s << endl;
+        //temp = nums[k-1];
+        //temp = temp+nums[((i+1)*k)-1]; 
+        //while(j<k)
+        //{
+            while(1)
             {
-                while(t > 0)
+                t = t+k;
+                if(t >= s)
                 {
-                    temp = nums[s-j];
-                    nums[s-j] = nums[s-j+k];
-                    nums[s-j+k] = temp;
-                    t--;j--;
+                    t=(t-s);
+                    //cout << t << endl;
                 }
-                a--;m++;
+                temp = temp+nums[t];
+                nums[t]=temp-nums[t];
+                temp = temp-nums[t];
+                //cout << temp;
+                if(t==j)
+                    break;
             }
-            if(m*k > s)
-            {
-                t = (s-((m-1)*k))-1;i=0;
-                while(t>=0)
-                {
-                    i = t;
-                    while(k>0)
-                    {
-                        temp = nums[i];
-                        nums[i] = nums[i+1];
-                        nums[i+1] = temp;
-                        k--;i++;
-                    }
-                    t--;
-                }
-                break;
-            }
-        }
+            //j++;
+            //temp = nums[j];
+          //  t = j;
+        //}
     }
 };
